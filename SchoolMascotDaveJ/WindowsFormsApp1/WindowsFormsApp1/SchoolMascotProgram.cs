40bcc4316a94090f60c2429e7f19d8c16f0f1081
DaveJanvier <dave.janvier@ocsbstudent.ca>
2018-02-15 13:42:47 -0500

    Updated the name of the form

--- SchoolMascotDaveJ/WindowsFormsApp1/WindowsFormsApp1/SchoolMascotProgram.cs
+++ SchoolMascotDaveJ/WindowsFormsApp1/WindowsFormsApp1/SchoolMascotProgram.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    static class Program
+    static class SchoolMascotProgram
     {
         /// <summary>
         /// The main entry point for the application.
@@ -16,7 +16,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmSchoolmascot());
         }
     }
 }
